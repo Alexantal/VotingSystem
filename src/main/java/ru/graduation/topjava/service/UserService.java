7e1bea948fea5ea0a31e5c9f4e831f1782bf1436
Alexantal <alexantal@yandex.ru>
2021-03-22 15:53:46 +0300

    10_Add common view layer, localization and UserService

---
+++ src/main/java/ru/graduation/topjava/service/UserService.java
@@ -0,0 +1,20 @@
+package ru.graduation.topjava.service;
+
+import org.springframework.stereotype.Service;
+import ru.graduation.topjava.model.User;
+import ru.graduation.topjava.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAll() {
+        return userRepository.getAll();
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
